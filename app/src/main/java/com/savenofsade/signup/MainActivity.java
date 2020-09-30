066b3f43f7d918b7729a391cfc993901c65e74d9
Amit <amit.immi85@gmail.com>
2020-09-30 13:25:28 +0530

    Connecting Remote

---
+++ app/src/main/java/com/savenofsade/signup/MainActivity.java
@@ -0,0 +1,27 @@
+package com.savenofsade.signup;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        setContentView(R.layout.activity_main);
+    }
+
+    //opening Signup Page
+    public void OpenSignupPage(View view) {
+        startActivity(new Intent(MainActivity.this,RegisterActivity.class));
+
+    }
+}
